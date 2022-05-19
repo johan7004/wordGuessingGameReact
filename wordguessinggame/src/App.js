c66edb2c10a8f9d5831e790eb89f923fa4ccb772
johan7004 <johanstephen7004@gmail.com>
2022-05-20 00:26:08 +0300

    added logic for guessing the word and hiding

--- wordguessinggame/src/App.js
+++ wordguessinggame/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState } from "react";
+import PlayerInput from "./components/player/PlayerInput";
+import "./App.css";
 
 function App() {
+  const [playerOneInput, setPlayerOneInput] = useState("");
+  const [playerTwoInput, setPlayerTwoInput] = useState("");
+  function playerInput1(e) {
+    e.preventDefault();
+    const inputValue = e.target;
+    setPlayerOneInput(inputValue.player1.value);
+  }
+
+  function playerInput2(e) {
+    e.preventDefault();
+    const inputValue = e.target;
+    setPlayerTwoInput(inputValue.player2.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
+        <div className="player1-input__container">
+          <p>Player 1</p>
+          <form onSubmit={playerInput1}>
+            <input name="player1" placeholder="Enter the Word"></input>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="player2-input__container">
+          <p>Player 2</p>
+          <PlayerInput
+            playerOneWord={playerOneInput}
+            playerTwoWord={playerTwoInput}
+          />
+          <form onSubmit={playerInput2}>
+            <input name="player2" placeholder="Guess The Word"></input>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ wordguessinggame/src/components/player/PlayerInput.js
@@ -0,0 +1,31 @@
+import { React, useState, useEffect } from "react";
+
+export default function PlayerInput(props) {
+  const inputWord = props.playerOneWord;
+  const [splittingWord, setSplittingWord] = useState(inputWord);
+
+  const [hiddenString, setHiddenString] = useState([]);
+
+  useEffect(() => {
+    setSplittingWord(inputWord);
+  }, [inputWord]);
+
+  useEffect(() => {
+    const words = splittingWord.split("");
+    setHiddenString(words);
+  }, [splittingWord]);
+
+  return (
+    <div>
+      {hiddenString.map((data, i) => {
+        return <p>{i % 2 === 0 ? data : "_"}</p>;
+      })}
+
+      {props.playerTwoWord === inputWord ? (
+        <p>you win</p>
+      ) : (
+        <p>Find the word or die</p>
+      )}
+    </div>
+  );
+}
